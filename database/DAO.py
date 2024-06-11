2cb8afefe89bb81313e61a1d4e13660b7dcbbf92
MattiaSernia294330 <mattiasernia.ms@gmail.com>
2024-06-11 13:12:51 +0200

    dale

--- database/DAO.py
+++ database/DAO.py
@@ -32,6 +32,10 @@ class DAO():
         query = """SELECT eo1.object_id as object_id1,  eo2.object_id as object_id2 
             from exhibition_objects eo1, exhibition_objects eo2
             where eo2.exhibition_id = eo1.exhibition_id and eo2.object_id != eo1.object_id"""
+        query_prof="""SELECT eo1.object_id as object_id1,  eo2.object_id as object_id2 
+            from exhibition_objects eo1, exhibition_objects eo2
+            where eo2.exhibition_id = eo1.exhibition_id and eo2.object_id > eo1.object_id
+            group by eo1.object_id,eo2.object_id"""
         cursor.execute(query)
         for row in cursor:
             result.append(Collegamento(row["object_id1"],row["object_id2"]))
